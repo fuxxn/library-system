a0c91d38d219175e02965015c1c4808ee136559c
Joe0406 <k55660921@gmail.com>
2026-06-18 14:28:49 +0800

    chore: 加入 Render 部署設定（render.yaml、process.env.PORT、engines）

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const classify = require('./decisionTree');
 const seed = require('./seed');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
